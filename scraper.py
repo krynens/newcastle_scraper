bb71911c70722de8a2c54ffecef45c44c876c9bf
Thom Krynen <75901402+krynens@users.noreply.github.com>
2021-03-07 08:46:56 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,38 +1,30 @@
+import scraperwiki
+import requests
+from datetime import datetime
+from bs4 import BeautifulSoup
 import os
 os.environ["SCRAPERWIKI_DATABASE_NAME"] = "sqlite:///data.sqlite"
 
-from bs4 import BeautifulSoup
-from datetime import datetime
-import requests
-import scraperwiki
 
 today = datetime.today()
 
-for i in range(1, 50):
-    try:
-        print(f'Getting page {i}')
-        url = 'https://www.melbourne.vic.gov.au/building-and-development/property-information/planning-building-registers/Pages/town-planning-permits-register-search-results.aspx?AdvertisingOnly=on&page=' + str(i)
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content, 'lxml')
+url = 'https://cn-web.t1cloud.com/T1PRDefault/WebApps/eProperty/P1/PublicNotices/AllPublicNotices.aspx?r=TCON.LG.WEBGUEST&f=P1.ESB.PUBNOTAL.ENQ'
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'lxml')
 
-        table = soup.find('tbody')
-        rows = table.find_all('tr', class_='detail')
+table = soup.find('div', id='ctl00_Content_cusApplicationResultsGrid_pnlCustomisationGrid')
+rows = table.find_all('table', class_='grid')
 
-        for row in rows:
-            record = {}
-            record['address'] = row.find('td', class_='column2').text
-            date_received_raw = row.find('td', class_='column3').text
-            record['date_received'] = datetime.strptime(
-                date_received_raw, "%d/%m/%Y").strftime("%Y-%m-%d")
-            record['date_scraped'] = today.strftime("%Y-%m-%d")
-            record['description'] = row.find('td', class_='column4').text
-            record['council_reference'] = row.find('td', class_='column1').text
-            record['info_url'] = 'https://www.melbourne.vic.gov.au' + \
-                str(row.find('td', class_='column1')).split('"')[5]
+for row in rows:
+    record = {}
+    record['address'] = row.find_all('td')[5].text
+    record['date_scraped'] = today.strftime("%Y-%m-%d")
+    record['description'] = row.find_all('td')[3].text
+    record['council_reference'] = row.find_all('td')[1].text
+    record['info_url'] = 'https://cn-web.t1cloud.com/T1PRDefault/WebApps/eProperty/P1/PublicNotices/' + \
+        str(row.find_all('td')[1]).split('"')[1]
+    on_notice_to_raw = row.find_all('td')[7].text
+    record['on_notice_to'] = datetime.strptime(on_notice_to_raw, '%d/%m/%Y').strftime("%Y-%m-%d")
 
-            scraperwiki.sqlite.save(
-                unique_keys=['council_reference'], data=record, table_name="data")
-
-    except:
-        print('Scraper finished.')
-        break
+    scraperwiki.sqlite.save(
+        unique_keys=['council_reference'], data=record, table_name="data")
